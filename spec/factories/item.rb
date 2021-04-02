a85bcda8a6cacb40ca3dcd8926a36f8fb780992d
Dominic Padula <domopadula2192@gmail.com>
2021-04-02 11:34:44 -0600

    factories setup

---
+++ spec/factories/item.rb
@@ -0,0 +1,10 @@
+require "faker"
+
+FactoryBot.define do
+  factory :item do
+    name { Faker::Name.unique.name }
+    description { "Best #1"}
+    unit_price { 10 }
+    merchant
+  end
+end
---
+++ spec/factories/transaction.rb
@@ -0,0 +1,10 @@
+require "faker"
+FactoryBot.define do
+  factory :transaction do
+  credit_card_number {Faker::Business.credit_card_number.to_s}
+  credit_card_expiration_date { "04/23" }
+   result { "success" }
+
+   invoice 
+  end
+end
