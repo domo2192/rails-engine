6ca27171b3b1874996faf2226db4db1de00a73cd
Dominic Padula <domopadula2192@gmail.com>
2021-04-06 17:45:05 -0600

    refactor collect records

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -4,8 +4,9 @@ class ApplicationRecord < ActiveRecord::Base
   def self.collect_records(per_page, page)
     per_page = (per_page || 20).to_i
     page = (page || 1).to_i
-
+    page = 1 if page < 1 
     limit(per_page).offset((page - 1) * per_page)
+
   end
 
   def self.find_record(id)
