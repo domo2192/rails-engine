ec235fd12e49798b085151b56fb036cd98b32691
Dominic Padula <domopadula2192@gmail.com>
2021-04-08 18:55:28 -0600

    refactor routes to clean up file

--- config/routes.rb
+++ config/routes.rb
@@ -1,46 +1,28 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   namespace :api do
     namespace :v1 do
+      get 'merchants/most_items', to: 'merchants#most_items'
       get 'revenue', to: 'revenue#between_dates'
+      namespace :merchants do
+        get '/find', to: 'search#find_one'
+      end
+      namespace :items do
+        get '/find_all', to: 'search#find_all'
+      end
       namespace :revenue do
         get '/merchants', to: 'revenue#most_revenue'
         get '/unshipped', to: 'revenue#unshipped_revenue'
         get '/merchants/:id', to: 'revenue#merchant_revenue'
         get 'items', to: 'revenue#item_revenue'
       end
-    end
-  end
-
-  #   get '/most_revenue', to: 'search#most_revenue'
-  namespace :api do
-    namespace :v1 do
-      namespace :merchants do
-        get '/find', to: 'search#find_one'
+      resources :items do
+        get '/merchant', to: 'items_merchant#show'
       end
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
-      namespace :items do
-        get '/find_all', to: 'search#find_all'
-      end
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      get 'merchants/most_items', to: 'merchants#most_items'
       resources :merchants do
         get '/items', to: 'merchant_items#show'
       end
     end
   end
-  namespace :api do
-    namespace :v1 do
-      resources :items do
-        get '/merchant', to: 'items_merchant#show'
-      end
-    end
-  end
-end
+end 
